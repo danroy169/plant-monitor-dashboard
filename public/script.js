fc472736d4ebbcb21e5fc33b323d028fc6368e51
danroy169 <danielwroy90@gmail.com>
2021-05-27 14:04:45 -0400

    initial commit

---
+++ public/script.js
@@ -0,0 +1,11 @@
+import { init } from './render.js'
+
+
+if(document.readyState === 'loading') { 
+    console.log('loading')
+    window.addEventListener('DOMContentLoaded', event => init())
+}
+else { 
+    init()
+}
+
