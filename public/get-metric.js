59e22245d9aa44f7d4cf641a371d354e9270394b
danroy169 <danielwroy90@gmail.com>
2021-05-27 15:55:58 -0400

    rm env

--- public/get-metric.js
+++ public/get-metric.js
@@ -1,6 +1,5 @@
-import { env } from './env.js'
 export async function getMetrics(metricID, amount){
-    const request = await fetch(env.host + env.api_port + '/api/metric/' + metricID + '/amount/' + amount, {mode: 'cors'})
+    const request = await fetch('http://localhost:3000/api/metric/' + metricID + '/amount/' + amount, {mode: 'cors'})
     
     const response = await request.json()
 
--- public/sse-setup.js
+++ public/sse-setup.js
@@ -1,6 +1,5 @@
-import { env } from './env.js'
 export default async function setupSSE() {
-    const evtSource = new EventSource(env.host + env.sse_port + '/sse')
+    const evtSource = new EventSource('http://localhost:3030/sse')
 
     const elements = {
         moisture1Reading: document.getElementById('moisture1'),
